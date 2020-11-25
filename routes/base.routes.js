99fee6ba7845237a84ccf8615f0142072a3e7ad0
cdvlpr <contacto.dvlpr@protonmail.com>
2020-11-25 16:16:41 +0100

    0.12-EventsBroken

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
-// Endpoints
+// const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('client/login', {
+//     errorMsg: 'Por favor, inicia sesión'
+// })
+// const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(req.client.role) ? next() : res.render('index', {
+//     errorMsg: 'No tienes los suficientes permisos'
+// })
+
+
+// Authenticated
 router.get('/', (req, res) => res.render('index'))
 
 
-
-
-
+// router.get('/perfil', ensureAuthenticated, checkRole(['Admin', 'Partner', 'Client']), (req, res) => res.render('profile', {
+//     user: req.user,
+//     isAdmin: req.user.role.includes('Admin')
+// }))
+// router.get('/partner', ensureAuthenticated, checkRole(['Admin', 'Partner']), (req, res) => res.render('partner/index', {
+//     user: req.user
+// }))
+// router.get('/management', ensureAuthenticated, checkRole(['Admin']), (req, res) => res.render('admin/index', {
+//     user: req.user
+// }))
 
 module.exports = router
--- routes/partner.routes.js
+++ routes/partner.routes.js
@@ -56,7 +56,7 @@ router.post('/signup', (req, res, next) => {
 
 router.get('/login', (req, res, next) => res.render('partner/login')) //{ errorMsg: req.flash("error") }))
 router.post('/partner/login', passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/:partner_id",
     failureRedirect: "/login",
     failureFlash: true,
     passReqToCallback: true
@@ -85,33 +85,24 @@ router.get('/logout', (req, res) => req.session.destroy((err) => res.redirect("/
 
 
 
+router.get('/', (req, res => res) 
 
+router.get('/:partner_id', (req, res, next) => {
 
-
-
-
-
-
-
-
-
-
-
-
-router.get('/', (req, res, next) => {
-    
-
-    const PartnerId = req.params._id
+    const
+        partnerId = req.params.partner_id
 
     Event
-        .find(PartnerId)
-        .then(allPartners => res.render('partner/index', {allPartners}))
+        .find({
+            'partner': ObjectId(partnerId)
+        })
+        // .populate('partner')
+        .then(console.log(partnerId))
+        .then(allPartnerEvents => res.render('partner/index', {
+            allPartnerEvents
+        }))
         .catch(err => next(new Error(err)))
 })
-
-
-
-
 
 
 
